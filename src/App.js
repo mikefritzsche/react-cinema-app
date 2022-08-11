53753fa57ffd7aa215301c2462c40a8a36beb3f8
Mike Fritzsche <mike@mikefritzsche.com>
2022-08-11 05:54:21 -0700

    add MainContent with slideshow

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useFetch } from './useFetch'
 import { Provider } from 'react-redux'
 import store from './redux/store'
 import Header from './components/header/Header'
+import Main from './components/main/Main';
 
 const App = () => {
   const [url, setUrl] = useState(null)
@@ -14,7 +15,7 @@ const App = () => {
     <Provider store={store}>
       <Header />
       <div className="app">
-        <h1>Setup React Redux</h1>
+        <Main/>
       </div>
     </Provider>
   );
